141656e8c6fce1521199bd2e33fe3e572e6bd713
Nathan LaPierre <nathanl2012@gmail.com>
2015-01-17 09:35:30 -0500

    added basic/placeholder account views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Login(request):
+	return render(request,'login.html')
+
+def Logout(request):
+	return render(request, 'index.html')

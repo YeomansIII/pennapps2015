b57865ad92bbc271941849ec4982ed15860a292b
Nathan LaPierre <nathanl2012@gmail.com>
2015-01-17 09:45:52 -0500

    removed my added account views in favor of django defaults

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,3 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def Login(request):
-	return render(request,'login.html')
-
-def Logout(request):
-	return render(request, 'index.html')

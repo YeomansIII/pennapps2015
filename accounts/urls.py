91318992e245e67eb90b0816c43ddfd8b2d6a21b
Jason D Yeomans <jyeoman2@gmu.edu>
2015-01-18 04:16:45 -0500

    register progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from accounts.views import DonorCreateView
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
     #url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url('^register/', DonorCreateView.as_view()),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,67 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import DonorUserFormSet, DonorAddressFormSet, DonorForm
+from accounts.models import Donor
 # Create your views here.
 
+class DonorCreateView(CreateView):
+    template_name = 'register.html'
+    model = Donor
+    form_class = DonorForm
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests and instantiates blank versions of the form
+        and its inline formsets.
+        """
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        donoruser_form = DonorUserFormSet()
+        donoraddress_form = DonorAddressFormSet()
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  donoruser_form=donoruser_form,
+                                  donoraddress_form=donoraddress_form))
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance and its inline
+        formsets with the passed POST variables and then checking them for
+        validity.
+        """
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        donoruser_form = DonorUserFormSet(self.request.POST)
+        donoraddress_form = DonorAddressFormSet(self.request.POST)
+        if (form.is_valid() and donoruser_form.is_valid() and
+            donoraddress_form.is_valid()):
+            return self.form_valid(form, donoruser_form, donoraddress_form)
+        else:
+            return self.form_invalid(form, donoruser_form, donoraddress_form)
+
+    def form_valid(self, form, donoruser_form, donoraddress_form):
+        """
+        Called if all forms are valid. Creates a Recipe instance along with
+        associated Ingredients and Instructions and then redirects to a
+        success page.
+        """
+        self.object = form.save()
+        donoruser_form.instance = self.object
+        donoruser_form.save()
+        donoraddress_form.instance = self.object
+        donoraddress_form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, donoruser_form, donoraddress_form):
+        """
+        Called if a form is invalid. Re-renders the context data with the
+        data-filled forms and errors.
+        """
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  donoruser_form=donoruser_form,
+                                  donoraddress_form=donoraddress_form))

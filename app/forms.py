3769d4bf1fa238e73cc2778a8db3d875668e88f7
Nathan LaPierre <nathanl2012@gmail.com>
2015-01-17 22:37:49 -0500

    added submit button to form

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from app.models import Pickup
 
 class PickupForm(forms.ModelForm):
     helper = FormHelper()
     helper.form_tag = False
+    self.helper.add_input(Submit('submit', 'Submit'))
 
     class Meta:
         model = Pickup
